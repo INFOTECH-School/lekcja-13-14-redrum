fd5a75d8f7a36e36849d7b7b2ecc27ae0df441a9
GaddlyDell <gabriel.krajewski@infotech.edu.pl>
2025-11-07 13:39:16 +0100

    jest g

--- fortnite/login/views.py
+++ fortnite/login/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from .forms import LoginForm, RegisterForm
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     form = LoginForm(request, data=request.POST or None)
     if request.method == 'POST' and form.is_valid():
         login(request, form.get_user())
@@ -10,9 +12,16 @@ def login_view(request):
     return render(request, 'login.html', {'form': form})
 
 def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     form = RegisterForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
         user = form.save()
         login(request, user)
         return redirect('home')
     return render(request, 'register.html', {'form': form})
+from django.contrib.auth import logout
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
